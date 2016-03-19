f70a3daab3d8a0d990c369198917dd8e1f1db5ce
memerelics <hash@hzl.jp>
2016-03-20 05:09:51 +0900

    move to lib

--- lib/jrq.rb
+++ lib/jrq.rb
@@ -1,5 +1,33 @@
 require "jrq/version"
 
+require 'json'
+require 'hashie'
+require 'awesome_print'
+
 module Jrq
-  # Your code goes here...
+
+  def self.run(args, opts)
+    _ = JSON.load(STDIN.read)
+    _ = Hashie::Mash.new(_)
+
+    if ARGV.length.zero?
+      display(_, opts)
+    else
+      display(eval(args.first), opts)
+    end
+
+  end
+
+  def self.display(o, options)
+    if o.is_a? Hash
+      puts JSON.pretty_generate(o)
+    else
+      if options.raw
+        puts o
+      else
+        ap o
+      end
+    end
+  end
+
 end
